c5b464c35e14fa7244ff1af400365aa8d5f700f2
jhiguera <j.higuera.f@gmail.com>
2016-03-25 19:22:41 +0000

    primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+asignaciones
+============
+
+A Symfony project created on March 18, 2016, 7:22 pm.
---
+++ src/JHF/UserBundle/JHFUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JHF\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JHFUserBundle extends Bundle
+{
+}
